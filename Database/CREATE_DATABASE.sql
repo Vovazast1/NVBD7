cd3e4d44141882d402e75b6c5c0a1bc72e38d76e
Vovazast1 <126915096+Vovazast1@users.noreply.github.com>
2026-01-04 16:42:17 +0200

    Create HR_Variant7 database if it doesn't exist

---
+++ Database/CREATE_DATABASE.sql
@@ -0,0 +1,12 @@
+-- =========================================
+-- CREATE DATABASE
+-- =========================================
+
+IF DB_ID('HR_Variant7') IS NULL
+BEGIN
+    CREATE DATABASE HR_Variant7;
+END
+GO
+
+USE HR_Variant7;
+GO
